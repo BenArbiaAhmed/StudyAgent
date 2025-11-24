a45507ebd846a7dc51a301469f6b4b8069746e06
BenArbiaAhmed <hamoudabenarbia15@gmail.com>
2025-11-24 23:17:30 +0100

    remove unused files

--- agents/mutil-purpose-agents/flashcards_agent.py
+++
@@ -1,9 +0,0 @@
-from langchain.agents import create_agent
-from middleware.dynamic_model import basic_model, dynamic_model_selection
-from response_format.flashcards_response_format import FlashcardDeck
-
-agent=create_agent(
-    model=basic_model,
-    middleware=[dynamic_model_selection],
-    tools=[],
-)
